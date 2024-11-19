2f0a68fa895fbf4d186ff8e822ee2d7353845441
𝔑𝔞𝔪 𝔑𝔤𝔲𝔶𝔢𝔫 <nguyenhoainam@hoppergames.io>
2024-11-19 13:20:18 +0700

    logcat sha1

--- UnityGoogleSignIn/src/main/java/com/ironygames/unitygooglesignin/GoogleLogin.java
+++ UnityGoogleSignIn/src/main/java/com/ironygames/unitygooglesignin/GoogleLogin.java
@@ -2,9 +2,15 @@ package com.ironygames.unitygooglesignin;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Log;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+import java.security.MessageDigest;
 
 public class GoogleLogin {
     public static String webClientId;
@@ -18,6 +24,11 @@ public class GoogleLogin {
         callbackSuccessFunc = successFunc;
         callbackFailFunc = failFunc;
 
+        String sha1 = GetSHA1CertificateFingerprint(activity.getPackageManager(), activity.getPackageName());
+        if( sha1 != null ) {
+            Log.e("Unity", String.format("SHA-1 is %s", sha1));
+        }
+
         Intent myIntent;
         if (useLegacy){
             myIntent = new Intent(activity, LegacyGoogleLoginActivity.class);
@@ -26,6 +37,35 @@ public class GoogleLogin {
             myIntent = new Intent(activity, GoogleLoginActivity.class);
         }
         activity.startActivity(myIntent);
+    }
+
+    private static String GetSHA1CertificateFingerprint(PackageManager packageManager, String packageName){
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+            for (Signature signature : packageInfo.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA-1");
+                md.update(signature.toByteArray());
+                return FormatSHA1(md.digest());
+            }
+            Log.e("Unity", "Get SHA-1 Certificate Fingerprint failed, error=no signature in package info");
+            return null;
+        }
+        catch (Exception e){
+            Log.e("Unity", "Get SHA-1 Certificate Fingerprint failed, check detail below");
+            Log.e("Unity", e.toString());
+            return null;
+        }
+    }
+
+    private static String FormatSHA1(byte[] sha1AsByteArr){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < sha1AsByteArr.length; i++) {
+            sb.append(String.format("%02X", sha1AsByteArr[i]));
+            if (i < sha1AsByteArr.length - 1) {
+                sb.append(":");
+            }
+        }
+        return sb.toString();
     }
 
     public static void Logout(Activity activity, String clientId, boolean useLegacy){
